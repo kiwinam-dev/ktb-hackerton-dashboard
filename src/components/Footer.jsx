b4958b67d55a6d1a7bf2e9d1404f824cec69e688
charlie.park.dev@gmail.com <mals930125@gmail.com>
2026-07-08 09:34:33 +0900

    fix: footer year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = ({ onAdminClick }) => {
 							카카오 테크 부트캠프
 						</div>
 						<p className="text-xs text-gray-500 dark:text-gray-400">
-							© 2025 Kakao Tech Bootcamp. All rights reserved.
+							© 2026 Kakao Tech Bootcamp. All rights reserved.
 						</p>
 					</div>
 
